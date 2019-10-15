fb31bf3782bfc4f888ccf2eba26cfa1ec270cc53
Nikhil Kulkarni <nikhil896@gmail.com>
2019-10-15 07:53:00 -0700

    Fetch friends

--- functions/src/FetchFriends.ts
+++ functions/src/FetchFriends.ts
@@ -2,6 +2,7 @@ import * as admin from 'firebase-admin';
 import * as functions from 'firebase-functions';
 import { Callable } from './Callable';
 import { USERS_COLLECTION, FRIENDS_COLLECTION } from './constants';
+import { DocumentSnapshot } from 'firebase-functions/lib/providers/firestore';
 
 export class FetchFriendsHandler extends Callable {
     
@@ -13,7 +14,51 @@ export class FetchFriendsHandler extends Callable {
             const id = decodedToken.uid;
             const db: admin.firestore.Firestore = admin.firestore();
 
-            db.collection(USERS_COLLECTION).doc(id).collection(FRIENDS_COLLECTION).doc()
+            db.collection(USERS_COLLECTION).doc(id).collection(FRIENDS_COLLECTION).doc().listCollections()
+            .then((result) => {
+                const phoneNumberList: string[] = [];
+                result.forEach((value) => {
+                    const phoneNumber: string = value.id
+                    phoneNumberList.push(phoneNumber);
+                })
+                const promiseArray: Array<Promise<DocumentSnapshot>> = []
+                phoneNumberList.forEach((phoneNumber) => {
+                    const query = db.collection(USERS_COLLECTION).doc(phoneNumber).get();
+                    promiseArray.push(query);
+                })
+                Promise.all(promiseArray)
+                .then((value) => {
+                    const friends: any[] = [];
+                    value.forEach((snapshot) => {
+                        const firstName = snapshot.get("firstName");
+                        const lastName = snapshot.get("lastName");
+                        const developerMetadata = snapshot.get("developerMetadata");
+                        const phoneNumber = snapshot.get("phoneNumber");
+
+                        const user = {
+                            firstName,
+                            lastName,
+                            developerMetadata,
+                            phoneNumber
+                        };
+                        friends.push(user);
+                    })
+
+                    const response = {
+                        friends,
+                    };
+
+                    res.send(response);
+                })
+                .catch((error) => {
+                    console.log('Failed batch promise, ' + error);
+                    res.status(500).send('Something went wrong');
+                })
+            })
+            .catch((error) => {
+                console.log('Failed fetching friend ids, ' + error);
+                res.status(500).send('Something went wrong');
+            })
         })
         .catch((error) => {
             console.log('Error decoding token, ' + error);
